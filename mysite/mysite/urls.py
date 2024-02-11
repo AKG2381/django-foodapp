d7560b89d245c3239868787677c7ea37a6de5b56
Ajeet Kumar Gupta <ajeetmalhar5@gmail.com>
2024-02-11 16:21:22 +0530

    changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',user_views.register,name='register'),
     path('food/', include('food.urls')),
     path('register/',user_views.register,name='register' ),
     path('login/',authentication_views.LoginView.as_view(template_name = 'users/login.html'),name='login' ),
