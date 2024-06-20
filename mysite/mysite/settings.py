5e0a63702fadc51d800e82428815ac6599863987
Ajeet Kumar Gupta <ajeetmalhar5@gmail.com>
2024-06-20 22:09:06 +0530

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4u)^^#8(g&zeged_(!k*l*&yt7zel@lpqnusk3g17b$5(ru-!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ajeetkgupta.pythonanywhere.com']
 
 
 # Application definition
