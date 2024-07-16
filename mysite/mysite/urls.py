0e2441d52efc39b191be1552231c50c7f90e8a20
Ajeet Kumar Gupta <ajeetmalhar5@gmail.com>
2024-07-16 19:55:42 +0530

    changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -33,6 +33,4 @@ urlpatterns = [
     path('profile/',user_views.profile_page,name='profile' ),
 ]
 
-urlpatterns += [
-    # ... the rest of your URLconf goes here ...
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
